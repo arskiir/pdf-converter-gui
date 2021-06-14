6476a7cd00d3b7697ca0c073057f69f9d1138bc5
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-06-14 18:03:12 +0700

    attepmted to add font

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -1,11 +1,35 @@
-<script>
+<script lang="ts">
+    const dpi = [6, 3, 1]
 </script>
 
-<main />
+<div class="title-bar">
+    <div class="title">
+        <span class="title-text accent-red">PDF</span>
+        <span class="title-text">Converter</span>
+    </div>
+</div>
 
 <style>
-    main {
+    .title-bar {
         width: 100%;
-        background-color: white;
+        display: flex;
+    }
+
+    .title {
+        display: flex;
+    }
+
+    .title-text {
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        line-height: 44px;
+        color: rgba(255, 255, 255, 0.95);
+    }
+
+    .title-text.accent-red {
+        color: rgba(255, 52, 52, 0.95);
+        margin-right: 10px;
     }
 </style>
